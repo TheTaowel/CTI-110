1259c379360d444e25f38b0a91f8370c9e9e13e7
TheTaowel <99230282+TheTaowel@users.noreply.github.com>
2023-09-26 14:18:46 -0400

    Add files via upload

---
+++ P1HW1_SpencerBrock.py
@@ -0,0 +1,14 @@
+#Spencer Brock
+#9/26/2023
+#Input, output, expression
+
+num1 = int(input("Enter a number: "))
+
+print("You entered: ", num1)
+print(num1, "squared: ", num1**2)
+print("And", num1, "cubed is: ", num1**3)
+
+num2 = int(input("Enter another number: "))
+print(num1, "+", num2, "=", num1+num2)
+print(num1, "*", num2, "=", num1*num2)
+
