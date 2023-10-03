09a44ea57e73da88c2a36b88ed023182cac7bf66
TheTaowel <99230282+TheTaowel@users.noreply.github.com>
2023-10-03 13:40:56 -0400

    Add files via upload

---
+++ P2LAB1_BrockSpencer.py
@@ -0,0 +1,10 @@
+milage = float (input())
+gascost = float (input())
+
+gallons = milage/20
+
+miles20= 20
+miles75= 75
+miles500= 500
+
+print((miles20/milage)* gascost, (miles75/milage)* gascost, (miles500/milage)* gascost)
