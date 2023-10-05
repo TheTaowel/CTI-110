021674a3f49c2ad893b4801337ae15b98bfe0c23
TheTaowel <99230282+TheTaowel@users.noreply.github.com>
2023-10-05 13:27:41 -0400

    Add files via upload

---
+++ P2HW1_SpencerBrock.py
@@ -0,0 +1,12 @@
+#Spencer Brock
+#10/5/2023
+# using f string 
+
+name = input("first name: ")
+hair = input("hair color: ")
+eye = input("eye color: ")
+height = input("height in feet: ")
+age = input("age: ")
+food= input("Favorite Food: ")
+
+print(f"{name} is a {height} foot tall student with {hair} hair and {eye} eyes. They are {age} years old and their favorite food is {food}.")
